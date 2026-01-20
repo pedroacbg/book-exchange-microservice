93a1f0580be802a7b1a821b261f6a6761eab61bf
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-01-20 09:22:15 -0300

    exchange service: fixing getExchange endpoint to retrive data from db

--- exchange-service/src/main/java/com/pedroacbg/controller/ExchangeController.java
+++ exchange-service/src/main/java/com/pedroacbg/controller/ExchangeController.java
@@ -2,6 +2,7 @@ package com.pedroacbg.controller;
 
 import com.pedroacbg.environment.InstanceInformationService;
 import com.pedroacbg.model.Exchange;
+import com.pedroacbg.repository.ExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +19,25 @@ public class ExchangeController {
     @Autowired
     private InstanceInformationService instanceInformationService;
 
-    @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @Autowired
+    private ExchangeRepository exchangeRepository;
+
+   /* @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to){
         return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT " + instanceInformationService.retrieveServerPort());
+    }*/
+
+    @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Exchange getExchange(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to){
+        Exchange exchange = exchangeRepository.findByFromAndTo(from, to);
+        if(exchange == null) throw new RuntimeException("Currency Unsupported!");
+
+        BigDecimal conversionFactor = exchange.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        exchange.setConvertedValue(convertedValue);
+        exchange.setEnvironment("PORT " + instanceInformationService.retrieveServerPort());
+
+        return exchange;
     }
 
 }
--- exchange-service/src/main/java/com/pedroacbg/repository/ExchangeRepository.java
+++ exchange-service/src/main/java/com/pedroacbg/repository/ExchangeRepository.java
@@ -2,7 +2,9 @@ package com.pedroacbg.repository;
 
 import com.pedroacbg.model.Exchange;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExchangeRepository extends JpaRepository<Exchange, Long> {
 
     Exchange findByFromAndTo(String from, String to);
