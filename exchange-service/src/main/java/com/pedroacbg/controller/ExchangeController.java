35efc51b13b5303e5a43779ad5f781f20af98797
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-01-19 21:27:38 -0300

    echange service: mocking getExchange endpoint

---
+++ exchange-service/src/main/java/com/pedroacbg/controller/ExchangeController.java
@@ -0,0 +1,21 @@
+package com.pedroacbg.controller;
+
+import com.pedroacbg.model.Exchange;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("exchange-service")
+public class ExchangeController {
+
+    @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Exchange getExchange(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to){
+        return new Exchange(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+    }
+
+}
