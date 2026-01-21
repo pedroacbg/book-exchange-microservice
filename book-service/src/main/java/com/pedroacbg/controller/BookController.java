00c5d984c58ab50aef5db2acedafc14be1ffef3a
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-01-21 19:00:05 -0300

    naming-server: creating eureka service

--- book-service/src/main/java/com/pedroacbg/controller/BookController.java
+++ book-service/src/main/java/com/pedroacbg/controller/BookController.java
@@ -11,11 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import javax.xml.crypto.Data;
-import java.util.Date;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
