a5798fcbbfaec43a10e4e482c25ab6d8d47cc58e
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-01-27 09:15:08 -0300

    book service: adding fall back controller & starting to implement swagger documentation

--- book-service/src/main/java/com/pedroacbg/controller/BookController.java
+++ book-service/src/main/java/com/pedroacbg/controller/BookController.java
@@ -5,6 +5,8 @@ import com.pedroacbg.environment.InstanceInformationService;
 import com.pedroacbg.model.Book;
 import com.pedroacbg.proxy.ExchangeProxy;
 import com.pedroacbg.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Tag(name = "Book Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class BookController {
@@ -32,23 +35,12 @@ public class BookController {
 //        return new Book(1L, "Nigeria Breno", "The new world against us", new Date(), 15.8, "BRL", "PORT " + port);
 //    }
 
+    @Operation(summary = "Find a specific book by your ID")
     @GetMapping(value = "/{id}/{currency}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Book getBook(@PathVariable(name = "id") Long id, @PathVariable(name = "currency") String currency){
         String port = instanceInformationService.retrieveServerPort();
         var book = bookRepository.findById(id).orElseThrow();
         ExchangeDTO exchange = exchangeProxy.getExchange(book.getPrice(), "USD", currency);
-
-        /*HashMap<String, String> params = new HashMap<>();
-        params.put("amount", book.getPrice().toString());
-        params.put("from", "USD");
-        params.put("to", currency);
-
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8000/exchange-service" + "/{amount}/{from}/{to}", ExchangeDTO.class, params);
-
-        ExchangeDTO exchange = response.getBody();*/
-
-        //book.setEnvironment("PORT " + port + " FEIGN");
         book.setEnvironment("BOOK PORT: " + port + "; EXCHANGE PORT: " + exchange.getEnvironment());
         book.setPrice(exchange.getConvertedValue());
         book.setCurrency(currency);
