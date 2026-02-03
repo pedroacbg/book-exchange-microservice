8edf7f26ae712d5380c3111d552329df76fb02fc
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2026-02-03 09:21:53 -0300

    connecting all services with zipkin

--- exchange-service/src/main/java/com/pedroacbg/controller/ExchangeController.java
+++ exchange-service/src/main/java/com/pedroacbg/controller/ExchangeController.java
@@ -5,6 +5,8 @@ import com.pedroacbg.model.Exchange;
 import com.pedroacbg.repository.ExchangeRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,8 @@ import java.math.BigDecimal;
 @RestController
 @RequestMapping("exchange-service")
 public class ExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(ExchangeController.class);
 
     @Autowired
     private InstanceInformationService instanceInformationService;
@@ -33,6 +37,7 @@ public class ExchangeController {
     @Operation(summary = "Get an exchange from amount of currency")
     @GetMapping(value = "/{amount}/{from}/{to}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Exchange getExchange(@PathVariable("amount") BigDecimal amount, @PathVariable("from") String from, @PathVariable("to") String to){
+        logger.info("getExchange is called with -> {}, {} and {}", amount, from, to);
         Exchange exchange = exchangeRepository.findByFromAndTo(from, to);
         if(exchange == null) throw new RuntimeException("Currency Unsupported!");
 
